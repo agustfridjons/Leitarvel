556cc966f23d2aca2ac54e1d43fdfa7dec54f9a9
JonThorir <jonthorir5@gmail.com>
2019-03-20 16:02:20 +0000

    Hotel

---
+++ HBVLeitarvel/src/functionality/Hotel.java
@@ -0,0 +1,78 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package functionality;
+
+/**
+ *
+ * @author Jón Þórir Sigurðarson
+ * @date
+ * Háskóli Íslands
+ */
+public class Hotel {
+    
+    private String name;
+    private int ratingStars;
+    private String location;
+    private int roomID;
+    private double price;
+    private boolean availability;
+    
+    public Hotel(){
+        
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getRatingStars() {
+        return ratingStars;
+    }
+
+    public void setRatingStars(int ratingStars) {
+        this.ratingStars = ratingStars;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public int getRoomID() {
+        return roomID;
+    }
+
+    public void setRoomID(int roomID) {
+        this.roomID = roomID;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public boolean isAvailability() {
+        return availability;
+    }
+
+    public void setAvailability(boolean availability) {
+        this.availability = availability;
+    }
+    
+    
+
+}
