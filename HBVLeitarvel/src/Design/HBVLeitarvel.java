b6c243630631df058d1aaa7998f3641052999ac3
agustfridjons <agf6@hi.is>
2019-03-27 15:50:20 +0000

    contr

--- HBVLeitarvel/src/Design/HBVLeitarvel.java
+++ HBVLeitarvel/src/Design/HBVLeitarvel.java
@@ -19,7 +19,7 @@ public class HBVLeitarvel extends Application {
     
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("FXMLSearch.fxml"));
         
         Scene scene = new Scene(root);
         
